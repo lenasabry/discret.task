1d79219d37df5eead99df9c75079778bf7813e32
lenasabry <121298761+lenasabry@users.noreply.github.com>
2022-12-23 20:08:01 +0200

    Add files via upload

---
+++ prefect3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class perfectnumbers
+{
+    public static void Main()
+    {
+        int n, i, sum;
+        int n1, n2;
+
+
+        Console.Write("enter the first number : ");
+        n1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("enter the second number: ");
+        n2 = Convert.ToInt32(Console.ReadLine());
+
+        for (n = n1; n <= n2; n++)
+        {
+            i = 1;
+            sum = 0;
+            while (i < n)
+            {
+                if (n % i == 0)
+                    sum = sum + i;
+                i++;
+            }
+            if (sum == n)
+                Console.Write("{0} ", n);
+        }
+        Console.Write("\n");
+    }
+}
---
+++ project prime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+internal partial class program
+{
+    private static void main(string[] args)
+    {
+        int num, i, x, y, z;
+        Console.Write("pleas enter number1:");
+        int y = int.Parse(Console.ReadLine());
+        y = y
+
+Console.Write("pleas enter number2:");
+        z = y
+
+    for (num = y; num <= z; num++) ;
+        {
+            x = 0;
+            for (i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    x++;
+                    break;
+                }
+            }
+            if (x == 0 && num != 1)
+                Console.Write("{0}", num);
+
+
+        }
+        {
+        }
+
